28f348b43c6ba5ba5910bb85def404d839cf42b3
Nahyun Lee <lnhyen43@gmail.com>
2023-12-21 21:46:45 +0900

    FEAT: styled compenents testing library에 설정

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "lib/testing-library-utils";
 import Page from "./page";
 
 test("initial condition", () => {
